2590b5ff38e1353a1244fe5c42b9ef089f9e95d8
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-20 15:32:11 -0300

    Dev database

--- eureka/settings.py
+++ eureka/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'eureka.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eureka_core',
+        'USER': 'uoe',
+        'PASSWORD': 'uoe1234',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
