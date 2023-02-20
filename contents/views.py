edf6f8f75f7b869833d67b87b53e18c75d49ba97
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-20 19:32:35 -0300

    Basic views for contents

--- contents/views.py
+++ contents/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Content
 
 
 def index(request):
     contents = Content.objects.order_by("-created_at")[:5]
-    output = f"These are the latest 5 contents: {[x.name for x in contents]}"
-    return HttpResponse(output)
+    context = {
+        "contents_list": contents,
+    }
+    # output = f"These are the latest 5 contents: {[x.name for x in contents]}"
+    return render(request, "contents/index.html", context)
+
+
+def detail(request, content_id):
+    try:
+        content = Content.objects.get(pk=content_id)
+    except Content.DoesNotExist:
+        raise Http404("Specified content does not exist")
+    return render(request, "contents/details.html", {"content": content})
