cc537697d7fc4da065316a88aa124420149688b9
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-21 15:40:35 -0300

    Add a test for the bk creation

--- contents/tests/test_bookmarks.py
+++ contents/tests/test_bookmarks.py
@@ -39,3 +39,38 @@ class BookmarkViewSetTestCase(APITestCase):
 
         self.assertEqual(len(data["results"]), 1)
         self.assertEqual(data["results"][0]["user"], user1.pk)
+
+    def test_user_can_bookmark_a_content(self):
+        user1 = User(
+            username="username1",
+            first_name="First",
+            last_name="Last",
+            password="password1234",
+            is_seller=True,
+        )
+        user1.save()
+        user2 = User(
+            username="username2",
+            first_name="First",
+            last_name="Last",
+            password="password1234",
+        )
+        user2.save()
+
+        content1 = Content(name="C1", description=".", user=user1)
+        content1.save()
+
+        self.client.force_authenticate(user=user1)
+        post_data = {
+            "user": user1.pk,
+            "content": content1.pk
+        }
+        response = self.client.post(self.BASE_URL, data=post_data, format="json")
+        self.assertEqual(response.status_code, 201)
+
+        data = response.json()
+        self.assertEqual(data["user"], user1.pk)
+        self.assertEqual(data["content"], content1.pk)
+
+        bk = Bookmark.objects.get(user=user1, content=content1)
+        self.assertIsNotNone(bk)
