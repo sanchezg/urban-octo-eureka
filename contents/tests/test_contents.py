0d75992e596df39e0b2e531f66647d154a6554f8
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-21 17:17:01 -0300

    Contents app factories

--- contents/tests/test_contents.py
+++ contents/tests/test_contents.py
@@ -1,53 +1,41 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
+from rest_framework.test import APIClient
 
+from contents.factories import ContentFactory
 from contents.models import Content
+from users.factories import UserFactory
 from users.models import User
 
 
 class ContentsViewTestCase(TestCase):
 
+    def setUp(self) -> None:
+        self.client = APIClient()
+        return super().setUp()
+
     def test_index_get_five_newest_contents(self):
-        client = Client()
         contents_index = reverse("contents:index")
 
-        response = client.get(contents_index)
+        response = self.client.get(contents_index)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"No contents are available.", response.content)
 
-        user = User(
-            username="username",
-            first_name="First",
-            last_name="Last",
-            password="password1234",
-            is_seller=True,
-        )
-        user.save()
+        # user = UserFactory()
+        contents = ContentFactory.create_batch(6)
 
-        for i in range(6):
-            Content.objects.create(user=user, name=f"Title{i}", description=".")  # pk starts in 1
-
-        response = client.get(contents_index)
+        response = self.client.get(contents_index)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"These are the last five contents uploaded:", response.content)
-        for i in range(1, 6):
-            self.assertIn(f"Title{i}".encode(), response.content)
+        # TODO: check the five items returned are the last ones
 
 
 class ContentsModelTestCase(TestCase):
 
     def test_user_consumer_cant_create_content(self):
 
-        user = User(
-            username="username",
-            first_name="First",
-            last_name="Last",
-            password="password1234",
-            is_seller=False,
-        )
-        user.save()
-
+        user = UserFactory()
         with self.assertRaises(ValidationError) as exc:
             Content.objects.create(user=user, name=f"Title", description=".")
         self.assertIn("Only consumer users can't create content. Please switch to a publisher account.", exc.exception.message)
