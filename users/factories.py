f1ef50b69f905661e02490aa577909ca54dd1bc6
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-21 16:28:18 -0300

    Added factory-boy. Fixed users tests

---
+++ users/factories.py
@@ -0,0 +1,19 @@
+import factory
+from factory.django import DjangoModelFactory
+
+from .models import User
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("username", "email",)
+
+    username = factory.Sequence(lambda n: f"fakeusername{n}")
+    email = factory.Sequence(lambda n: f"fakeemail{n}@email.com")
+    first_name = "John"
+    last_name = "Rambo"
+
+
+class SellerUserFactory(UserFactory):
+    is_seller = True
