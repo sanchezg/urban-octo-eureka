34b6f2bcb64796036ac1d625bf3914cb362ad2b7
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-21 00:32:01 -0300

    Test some logic (#2)

--- contents/tests.py
+++ contents/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Content
+from users.models import User
+
+
+class ContentsViewTestCase(TestCase):
+
+    def test_index_get_five_newest_contents(self):
+        client = Client()
+        contents_index = reverse("contents:index")
+
+        response = client.get(contents_index)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"No contents are available.", response.content)
+
+        user = User(
+            username="username",
+            first_name="First",
+            last_name="Last",
+            password="password1234",
+            is_seller=True,
+        )
+        user.save()
+
+        for i in range(6):
+            Content.objects.create(user=user, name=f"Title{i+1}", description=".")  # pk starts in 1
+
+        response = client.get(contents_index)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"These are the last five contents uploaded:", response.content)
+        for i in range(2, 7):
+            self.assertIn(f"<li><a href=\"/contents/{i}/\">Title{i}".encode(), response.content)
+
+
+class ContentsModelTestCase(TestCase):
+
+    def test_user_consumer_cant_create_content(self):
+
+        user = User(
+            username="username",
+            first_name="First",
+            last_name="Last",
+            password="password1234",
+            is_seller=False,
+        )
+        user.save()
+
+        with self.assertRaises(ValidationError) as exc:
+            Content.objects.create(user=user, name=f"Title", description=".")
+        self.assertIn("Only consumer users can't create content. Please switch to a publisher account.", exc.exception.message)
--- contents/urls.py
+++ contents/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import index, detail
 
+app_name = "contents"
 urlpatterns = [
     path("", index, name="index"),
     path("<int:content_id>/", detail, name="details")
