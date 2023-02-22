c64517fbba64e609a3fe2562fd8e9822d3b84eb3
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-21 23:27:34 -0300

    More tests

--- contents/tests/test_reviews.py
+++ contents/tests/test_reviews.py
@@ -1,16 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIClient
 
 from contents.factories import ContentFactory, ContentReviewFactory
+from users.factories import UserFactory
 
 class ContentReviewViewSetTestCase(TestCase):
 
+    BASE_URL = reverse("review-list")
+
     def setUp(self) -> None:
         self.client = APIClient()
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+        self.content = ContentFactory()
         return super().setUp()
 
     def test_user_can_review_content(self):
-        pass
+        request_data = {
+            "user": self.user.pk,
+            "content": self.content.pk,
+            "review": "great content!",
+            "score": 5,
+        }
+
+        response = self.client.post(self.BASE_URL, data=request_data, format="json")
+
+        self.assertEqual(response.status_code, 201)
+        data = response.json()
+        self.assertEqual(data["user"], self.user.pk)
+        self.assertEqual(data["content"], self.content.pk)
+        self.assertEqual(data["review"], "great content!")
+        self.assertEqual(data["score"], 5)
 
     def test_publisher_cant_review_own_content(self):
         pass
