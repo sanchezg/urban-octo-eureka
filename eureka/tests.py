17d064779ee1e04c3188782b31526e5038303cda
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-23 15:45:58 -0300

    Improve these tests

--- eureka/tests.py
+++ eureka/tests.py
@@ -7,19 +7,17 @@ from users.factories import UserFactory
 
 class AuthTestCase(TestCase):
     def setUp(self) -> None:
-        password = "abcdef102938"
-
         self.client = APIClient()
-        self.user = UserFactory()
-        # self.user.set_password(password)
-        # self.user.save()
-        self.user_data = {"username": self.user.username, "password": password}
         self.login_url = reverse("knox_login")
         self.logout_url = reverse("knox_logout")
         return super().setUp()
 
     def test_logged_in_user_can_access_auth_views(self):
-        response = self.client.post(self.login_url, data=self.user_data, format="json")
+        password = "abcdef102938"
+        user = UserFactory(password=password)
+        user_data = {"username": user.username, "password": password}
+
+        response = self.client.post(self.login_url, data=user_data, format="json")
 
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
@@ -31,11 +29,17 @@ class AuthTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_logged_out_user_cant_access_auth_views(self):
-        self.client.login(**self.user_data)  # login
-        self.client.post(self.login_url, data=self.user_data, format="json")  # logout
+        password = "abcdef102938"
+        user = UserFactory(password=password)
+        user_data = {"username": user.username, "password": password}
+        self.client.login(**user_data)  # login
+        self.client.post(self.login_url, data=user_data, format="json")  # logout
 
         response = self.client.get(reverse("user-list"))
         self.assertEqual(response.status_code, 401)
 
     def test_anon_user_can_access_index(self):
-        pass
+        self.client.credentials()  # Override existing ones (if there are some)
+        response = self.client.get(reverse("contents:index"))
+
+        self.assertEqual(response.status_code, 200)
