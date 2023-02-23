cbef17f986ab16adfa37c1dd31c37bf47f2d7671
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-02-23 11:11:53 -0300

    Enable api auth with knox

--- eureka/settings.py
+++ eureka/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Third party
     "rest_framework",
     "django_extensions",
+    "knox",  # Auth
     # Site specific apps
     "users.apps.UsersConfig",
     "contents.apps.ContentsConfig",
@@ -133,4 +134,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
-REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination", "PAGE_SIZE": 10}
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": ["knox.auth.TokenAuthentication"],
+}
