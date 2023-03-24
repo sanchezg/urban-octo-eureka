196125e48c9e3d2d1be160cb8fa56bcc9eaa3182
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-03-24 17:01:28 -0300

    Add missing db name

--- eureka/settings.py
+++ eureka/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "eureka.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
+        "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
